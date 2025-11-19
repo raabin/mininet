2288f712b2a85523dec75aae56a012af02781f23
Rabin Pandey <rabinpandey181@gmail.com>
2025-11-18 23:40:13 -0600

    debug

--- util/install.sh
+++ util/install.sh
@@ -168,32 +168,23 @@ function mn_deps {
         $install ${PYPKG}-pyflakes pylint ${PYPKG}-pep8-naming \
             ${PYPKG}-pexpect
     elif [ "$DIST" = "SUSE LINUX"  ]; then
+        echo "inside suse linux"
 		$install gcc make socat psmisc xterm openssh iperf \
 			iproute telnet ${PYPKG}-setuptools libcgroup-tools \
 			ethtool help2man python-pyflakes python3-pylint \
                         python-pep8 ${PYPKG}-pexpect ${PYPKG}-tk
     else  # Debian/Ubuntu
-        pf=pyflakes
-        pep8=pep8
-        # Starting around 20.04, installing pyflakes instead of pyflakes3
-        # causes Python 2 to be installed, which is exactly NOT what we want.
-        if [ "$DIST" = "Ubuntu" -a `expr $RELEASE '>=' 20.04` = "1" ]; then
-                pf=pyflakes3
-        fi
-        # Debian 11 "bullseye" renamed
-        # * pep8 to python3-pep8
-        # * pyflakes to pyflakes3
-        if [ "$DIST" = "Debian" -a `expr $RELEASE '>=' 11` = "1" ]; then
-                pf=pyflakes3
-                pep8=python3-pep8
-        fi
-
+        echo "Inside Debian/Ubuntu"
+        pf=python3-pyflakes
+        pep8=python3-pep8
+        echo "After pyflakes/pep8"
         $install gcc make socat psmisc xterm ssh iperf telnet \
-                 ethtool help2man $pf pylint $pep8 \
+                 ethtool help2man python3-pyflakes pylint python3-pep8 \
                  net-tools ${PYPKG}-tk
-
+        echo "After net-tools"
         # Install pip
         $install ${PYPKG}-pip || $install ${PYPKG}-pip-whl
+        echo "After pip install"
         if ! ${PYTHON} -m pip -V; then
             if [ $PYTHON_VERSION == 2 ]; then
                 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
@@ -204,14 +195,18 @@ function mn_deps {
             rm get-pip.py
         fi
        ${python} -m pip install pexpect
+        echo "After pexpect install"
         $install iproute2 || $install iproute
+        echo "After iproute2 install"
         $install cgroup-tools || $install cgroup-bin
+        echo "After cgroup-tools install"
         $install cgroupfs-mount
-    fi
+        echo "After cgroupfs-mount install"
 
     echo "Installing Mininet core"
     pushd $MININET_DIR/mininet
     sudo PYTHON=${PYTHON} make install
+    echo "After Mininet core install"
     popd
 }
 
@@ -777,15 +772,21 @@ function all {
     mn_deps
     # Skip mn_doc (doxypy/texlive/fonts/etc.) because it's huge
     # mn_doc
+    echo "before of"
     of
+    echo "after of"
     install_wireshark
+    echo "after wireshark"
     ovs
+    echo "after ovs"
     # We may add ivs once it's more mature
     # ivs
     # NOX-classic is deprecated, but you can install it manually if desired.
     # nox
     pox
+    echo "after pox"
     oftest
+    echo "after oftest"
     cbench
     echo "Enjoy Mininet!"
 }
